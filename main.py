871f7d6b6ffd587233729d22548559a5f1707e77
Georges Mathieu <gmathieu6@gmail.com>
2025-07-29 22:09:08 -0400

    work to get used to paizo naming convention

--- main.py
+++ main.py
@@ -3,22 +3,36 @@ import sys
 import re
 
 def main():
-    print(sys.argv)
+    # print(sys.argv)
     directory = sys.argv[1]
 
-    for item in os.listdir(directory):
-        name = item.split(".", 1)
-
-        print(name)
-        if len(name) == 1:
-            print(f"Directory: {name}")
-        elif name[1] == "pdf":
-            print(f"Pdf in root: {item}")
-        else:
-            print(f"Not a pdf: {item}")
+    if os.path.isdir(directory):
+        directorySearch(directory)
+    else:
+        # print("Invalid input")
+        return 1
 
     return 0
 
+def directorySearch(dirPath):
+    for itempath in os.listdir(dirPath):
+        name = itempath.split(".",1)
+        if len(name) == 1:
+            # print(f"Directory: {name}")
+            directorySearch(f"{dirPath}\\{itempath}")
+        elif name[1] == "pdf":
+            # print(f"Pdf: {itempath}")
+            processPDF(dirPath, itempath)
+        else:
+            # print(f"Not a pdf: {itempath}")
+            return
+
+
+def processPDF(dirPath, item):
+    if re.match(r"PZO[A-Z0-9](5,6) .+", item) is not None:
+        return
+    print(f"{dirPath}\\{item}")
+    return
 
 
 if __name__ == "__main__":
